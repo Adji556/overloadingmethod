40cdfc8be9c2b435b34732cbaf04a630e3532acd
Adji556 <80576139+Adji556@users.noreply.github.com>
2021-04-01 16:41:46 +0700

    Add files via upload

---
+++ Number.cs
@@ -0,0 +1,77 @@
+﻿class Numbers
+{
+    public int findMinimum(int a, int b)
+    {
+        if (a < b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+
+    }
+    public int findMinimum(int a, int b, float c)
+    {
+
+        if (a < b)
+        {
+            if (a < c)
+            {
+                return a;
+            }
+            else
+            {
+                return (int)c;
+            }
+        }
+        else
+        {
+            if (b < c)
+            {
+                return b;
+            }
+            else
+            {
+                return (int)c;
+            }
+        }
+    }
+    public int findMaximum(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+    public int findMaximum(int a, int b, float c)
+    {
+        if (a > b)
+        {
+            if (a > c)
+            {
+                return a;
+            }
+            else
+            {
+                return (int)c;
+            }
+        }
+        else
+        {
+            if (b > c)
+            {
+                return b;
+            }
+            else
+            {
+                return (int)c;
+            }
+        }
+    }
+}
